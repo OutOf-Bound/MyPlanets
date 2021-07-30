1f1d5118774dbaafcc40e972dac0cdcbd030614e
RealVolAnd <out_of_bound@yahoo.com>
2021-07-30 16:30:33 +0300

    Animation added

--- app/src/main/java/net/smartgekko/myplanets/views/MainActivity.kt
+++ app/src/main/java/net/smartgekko/myplanets/views/MainActivity.kt
@@ -1,24 +1,20 @@
 package net.smartgekko.myplanets.views
 
-import android.graphics.drawable.Drawable
+import android.graphics.Rect
 import android.os.Bundle
+import android.view.Gravity
 import android.view.View
 import android.webkit.WebSettings
 import android.webkit.WebViewClient
-import android.widget.ImageView
 import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.constraintlayout.widget.Group
-import androidx.transition.Transition
-import androidx.transition.TransitionInflater
-import androidx.transition.TransitionManager
+import androidx.transition.*
 import com.google.android.material.appbar.AppBarLayout
 import net.smartgekko.myplanets.R
 import net.smartgekko.myplanets.databinding.ActivityMainBinding
 import net.smartgekko.myplanets.utils.*
-import net.smartgekko.myplanets.utils.ONE_PLANET_STATE
 
 
 class MainActivity : AppCompatActivity() {
@@ -49,6 +45,7 @@ class MainActivity : AppCompatActivity() {
         binding.sun.setOnClickListener {
             letsShowInfo(getString(R.string.sun), "")
         }
+
         binding.appbar.addOnOffsetChangedListener(object : AppBarStateChangeListener() {
             override fun onStateChanged(appBarLayout: AppBarLayout?, state: State?) {
                 when (state) {
@@ -64,14 +61,14 @@ class MainActivity : AppCompatActivity() {
                     }
                 }
             }
-        }
-        )
+        })
 
         binding.planetsGroup.setAllOnClickListener(View.OnClickListener {
             val nameText: TextView = it.findViewWithTag("planetName")
             letsShowOnePlanet(nameText.text.toString())
 
         })
+
         binding.sun.setOnClickListener {
             letsShowOnePlanet(resources.getString(R.string.sun))
         }
@@ -82,10 +79,11 @@ class MainActivity : AppCompatActivity() {
 
         binding.smallTumb.setOnClickListener {
             letsShowAllPlanets()
-            binding.appbar.setExpanded(true,true)
+            binding.appbar.setExpanded(true, true)
         }
 
         letsShowInfo(getString(R.string.jupiter), " (планета)")
+
     }
 
     private fun Group.setAllOnClickListener(listener: View.OnClickListener?) {
@@ -94,21 +92,19 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-
     private fun letsShowInfo(currentPlanet: String, extendedString: String) {
         binding.searchWebView.loadUrl(WIKI_BASE_URL + currentPlanet + extendedString)
+        val slide = Slide()
+        slide.slideEdge = Gravity.BOTTOM
+        slide.setDuration(300)
+        binding.textPlanet.visibility = View.INVISIBLE
+        TransitionManager.beginDelayedTransition(binding.titleLayout,slide)
         binding.textPlanet.text = currentPlanet
-    }
-
-    private fun letsShowSolarSystemInfo() {
-        binding.searchWebView.loadUrl(WIKI_BASE_URL + getString(R.string.solar_system))
-        binding.textPlanet.text = getString(R.string.solar_system)
+        binding.textPlanet.visibility = View.VISIBLE
     }
 
     private fun letsShowOnePlanet(onePlanetName: String) {
         var extendString = ""
-
-        binding.planetsLayout.visibility = View.INVISIBLE
         when (onePlanetName) {
             resources.getString(R.string.sun) -> {
                 binding.onePlanetImage.setImageResource(R.drawable.sun)
@@ -133,22 +129,29 @@ class MainActivity : AppCompatActivity() {
             }
             resources.getString(R.string.uranus) -> {
                 binding.onePlanetImage.setImageResource(R.drawable.uranus)
-                extendString =" (планета)"
+                extendString = " (планета)"
             }
             resources.getString(R.string.neptune) -> {
                 binding.onePlanetImage.setImageResource(R.drawable.neptune)
             }
         }
+
         binding.textPlanet.text = onePlanetName
-        binding.onePlanet.visibility = View.VISIBLE
-        letsShowInfo(onePlanetName,extendString)
+
+        TransitionManager.beginDelayedTransition(binding.planetsLayout,Fade().setDuration(300))
+        TransitionManager.beginDelayedTransition(binding.onePlanet,Fade().setDuration(300))
+        hideSolarSystem()
+        showOnePlanet()
+        letsShowInfo(onePlanetName, extendString)
         setCurrentState(ONE_PLANET_STATE)
     }
 
     private fun letsShowAllPlanets() {
-        binding.onePlanet.visibility = View.INVISIBLE
-        binding.planetsLayout.visibility = View.VISIBLE
-        letsShowSolarSystemInfo()
+        TransitionManager.beginDelayedTransition(binding.onePlanet,Fade().setDuration(300))
+        TransitionManager.beginDelayedTransition(binding.planetsLayout,Fade().setDuration(300))
+        hideOnePlanet()
+        showSolarSystem()
+        letsShowInfo(getString(R.string.solar_system), "")
         setCurrentState(MULTI_PLANET_STATE)
     }
 
@@ -165,13 +168,57 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    private fun checkAndSetCurrentState(){
+    private fun checkAndSetCurrentState() {
         if (onePlanetView) {
             setCurrentState(ONE_PLANET_STATE)
         } else {
             setCurrentState(MULTI_PLANET_STATE)
         }
     }
+
+    private fun showSolarSystem(){
+        with(binding){
+            the1.visibility = View.VISIBLE
+            the2.visibility = View.VISIBLE
+            the3.visibility = View.VISIBLE
+            the4.visibility = View.VISIBLE
+            the5.visibility = View.VISIBLE
+            the6.visibility = View.VISIBLE
+            the7.visibility = View.VISIBLE
+            the8.visibility = View.VISIBLE
+            sun.visibility = View.VISIBLE
+        }
+    }
+
+    private fun hideSolarSystem(){
+        with(binding){
+            the1.visibility = View.INVISIBLE
+            the2.visibility = View.INVISIBLE
+            the3.visibility = View.INVISIBLE
+            the4.visibility = View.INVISIBLE
+            the5.visibility = View.INVISIBLE
+            the6.visibility = View.INVISIBLE
+            the7.visibility = View.INVISIBLE
+            the8.visibility = View.INVISIBLE
+            sun.visibility = View.INVISIBLE
+        }
+    }
+
+    private fun showOnePlanet(){
+        with(binding){
+            onePlanet.visibility = View.VISIBLE
+        }
+    }
+
+    private fun hideOnePlanet(){
+        with(binding){
+            onePlanet.visibility = View.INVISIBLE
+        }
+    }
+
+
+
+
 }
 
 
